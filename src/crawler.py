bcb5e00aea0aecd23617d2f2ae4b5a2f5e6ab181
Bene H <benehartl22@gmail.com>
2026-05-08 14:02:42 +0200

    Crawler Hauptskript Konstruktor

---
+++ src/crawler.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
+from .models import CrawlerConfig
+
+
+class Crawler:
+    
+    def __init__(self,CrawlerConfig):
+        self.config = CrawlerConfig()
