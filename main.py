500eda4c85223a185b00de771f2a8faeade637c6
Bene H <benehartl22@gmail.com>
2026-08-05 13:22:29 +0200

    Bsp mit Spiegel Test

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from core.browser_controller import BrowserController
 
 async def testcrawl():
     async with BrowserController(headless = False, allow_3p = False) as bc:
-        context = await bc.new_context("https://www.google.de")
+        context = await bc.new_context("https://www.spiegel.de/lebenundlernen/schule/familienhund-vater-schliesst-vertrag-mit-seinen-kindern-a-1105596.html")
         print(await context.cookies())
         page = await context.new_page()
-        visti_page = await page.goto("https://www.google.de")
+        visti_page = await page.goto("https://www.spiegel.de/lebenundlernen/schule/familienhund-vater-schliesst-vertrag-mit-seinen-kindern-a-1105596.html")
         await asyncio.sleep(3) 
         #print(await page.content())
         if visti_page:
