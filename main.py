952fba64de69b787dbfd4cce04e1cc92fcf91584
Bene H <benehartl22@gmail.com>
2026-08-05 10:18:57 +0200

    Verbessrung main.py testfunktion

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import asyncio
 from core.browser_controller import BrowserController
 
 async def testcrawl():
-    async with BrowserController(headless = False, allow_3p = True) as bc:
+    async with BrowserController(headless = False, allow_3p = False) as bc:
         context = await bc.new_context("https://www.google.de")
         page = await context.new_page()
-        await page.goto("https://www.google.de")
+        visti_page = await page.goto("https://www.google.de")
         await asyncio.sleep(3) 
-        print(await page.content())
-        
+        #print(await page.content())
+        if visti_page:
+            print (await visti_page.request.all_headers())
+            print (await visti_page.all_headers())
+            
         await context.close()
         
         
