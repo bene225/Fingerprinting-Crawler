6f37970044ffec5ad1c5652008725bfddaaab95b
Bene H <benehartl22@gmail.com>
2026-08-05 13:20:56 +0200

    Hinzu: Cookie speicher auslesen nicht nur Header

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from core.browser_controller import BrowserController
 async def testcrawl():
     async with BrowserController(headless = False, allow_3p = False) as bc:
         context = await bc.new_context("https://www.google.de")
+        print(await context.cookies())
         page = await context.new_page()
         visti_page = await page.goto("https://www.google.de")
         await asyncio.sleep(3) 
         #print(await page.content())
         if visti_page:
+            print("-------------------------")
             print (await visti_page.request.all_headers())
             print (await visti_page.all_headers())  
+            print("-----------------------------")
+            print(await context.cookies())
         await context.close()
         
         
