e729974c2921b4451fff30597c53d7910a78befa
Bene H <benehartl22@gmail.com>
2026-08-07 16:29:27 +0200

    Hinzugefügt:_appen_event und monkeypatch.js

--- core/injector.py
+++ core/injector.py
@@ -16,9 +16,12 @@ class Injector:
         self.events : list[DetectedFingerprint] = []
     
     async def integrade_monkeypatch(self, page : Page) -> None:
-        # Zählfunktion verfügbar machen
-        await page.expose_binding("register_fp", _append_event)
+        # Zählfunktion verfügbar machen (nicht ausführen)
+        await page.expose_binding("register_fp", self._append_event)
         # Skript einlesen und in Website einfügen
         with open("monkeypatch.js", "r", encoding="utf-8") as mp_script:
             mp_script = mp_script.read()
         await page.add_init_script(mp_script)
+        
+    async def _append_event(self, _, api, method, page_url) -> None:
+        self.events.append(DetectedFingerprint(type=api, method=method,url=page_url))
