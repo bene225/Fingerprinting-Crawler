c8009999a0933d7ba5c234a58465ae7065ed8af9
Bene H <benehartl22@gmail.com>
2026-08-09 23:31:07 +0200

    Hinzugefügt MP + Injector fertig

---
+++ core/monkeypatch.js
@@ -0,0 +1,7 @@
+// Immer aufrufen
+(() => {
+    function register_fp(api,method){
+        // Aufruf der Einbinung injector/register_fp in das JS
+        window.register_fp(api, method, location.href)
+    }
+});
