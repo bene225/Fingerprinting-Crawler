d64e6f5ae2dcca10b8c8db4c00b2ff6351aca077
Bene H <benehartl22@gmail.com>
2026-08-11 17:54:56 +0200

    Überschriebung AudioKosnt vererbung außerhalb fkt

--- core/monkeypatch.js
+++ core/monkeypatch.js
@@ -44,9 +44,10 @@
         const fp_original_function = window[audio_constructor]
         window[audio_constructor] = function wrapper_and_original_fp (...args){
             register_fp("webaudio", audio_constructor);
-            return new window[audio_constructor];
             // Neue fkt muss wieder Instanzen vom Original erzeugen
-            window[audio_constructor].prototype = fp_original_function.prototype;            
+            window[audio_constructor].prototype = fp_original_function.prototype;
+            return new window[audio_constructor];
+                        
         }
     }
 });
