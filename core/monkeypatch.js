d707f83f22a2c7eec8254f28ae25456bdaee6a80
Bene H <benehartl22@gmail.com>
2026-08-11 17:09:56 +0200

    Audio Wrapper fertig

--- core/monkeypatch.js
+++ core/monkeypatch.js
@@ -17,16 +17,26 @@
     canvas_methods.forEach((method) => {
         if (HTMLCanvasElement.prototype[method]){
             //https://developer.mozilla.org/de/docs/Web/API/HTMLCanvasElement#instanzmethoden
-            const fp_original_function = HTMLCanvasElement.prototype[method]
+            const fp_original_function = HTMLCanvasElement.prototype[method];
             // Wrapper zu fp_o_f
             HTMLCanvasElement.prototype[method] = function wrapper_and_original_fp(...args){
-                register_fp(canvas ,method)
-                return fp_original_function.apply(this, args)
+                register_fp("canvas" ,method);
+                return fp_original_function.apply(this, args);
             }
         }
     })
     
-    //MP für Audio
+    //MP für Audio 
+    const audio_methods = ["getChannelData"];
+    audio_methods.forEach((method) => {
+        if (AudioBuffer.prototype[method]){
+            const fp_original_function = AudioBuffer.prototype[method];
+            AudioBuffer.prototype[method] = function wrapper_and_original_fp(...args){
+                register_fp("audio", method);
+                return fp_original_function.apply(this, args);
+            }
 
+        }
+    })
 
 });
