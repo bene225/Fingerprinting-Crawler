07c119064a38bcfe350febfa54d07dfe85c00a07
Bene H <benehartl22@gmail.com>
2026-08-16 16:39:08 +0200

    rebase fertig

--- core/monkeypatch.js
+++ core/monkeypatch.js
@@ -46,8 +46,10 @@
         window[audio_constructor] = function wrapper_and_original_fp (...args){
             register_fp("webaudio", audio_constructor);
             // Neue fkt muss wieder Instanzen vom Original erzeugen
+
             return new fp_original_function(...args);
             window[audio_constructor].prototype = fp_original_function.prototype;            
+
         }
     }
 })();
