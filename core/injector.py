474786c7c9aa76bf521db425fb7bd2788732efb6
Bene H <benehartl22@gmail.com>
2026-09-08 13:10:12 +0200

    Inint Script Website durchreiche und js_cooke_block

--- core/injector.py
+++ core/injector.py
@@ -24,13 +24,19 @@ class Injector:
         with open("core/monkeypatch.js", "r", encoding="utf-8") as mp_script:
             mp_script = mp_script.read()
         await page.add_init_script(mp_script)
+        
+    async def integrade_js_cookie_block(self, url_uncut : str, page : Page) -> None:
+        escaped_url = first_party_url_to_js(url_uncut)
+        with open("core/js_cookie_block.js", encoding="utf-8") as js_cookie_block_file:
+            js_cookie_block_script = js_cookie_block_file.read()
+            await page.add_init_script(f"window._location_url = {escaped_url};\n" + js_cookie_block_script)
     
     # Aufruf auf Website duch mp wenn Fp erkannt, dann zählen
     # location href aus der mp.js
     async def _append_event(self, _, api, method, location_href) -> None:
         self.events.append(DetectedFingerprint(api=api, method=method,url=location_href))
         
-async def first_party_url_to_js(url :str, page: Page) -> None:
-    cutted_url = origin_domain(url_uncut=url)
+def first_party_url_to_js(url_uncut :str) -> str:
+    cutted_url = origin_domain(url_uncut=url_uncut)
     escaped_url = json.dumps(cutted_url)
-    await page.add_init_script(f"window._location_url = {escaped_url}")
+    return escaped_url
