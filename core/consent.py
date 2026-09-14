c510bda95590d6cd0febb1fcdd673acf5a1563be
Bene H <benehartl22@gmail.com>
2026-09-14 19:13:16 +0200

    Kommentare zu consent

--- core/consent.py
+++ core/consent.py
@@ -1,3 +1,7 @@
 import asyncio
 from playwright.async_api import async_playwright
 import random
+
+# Wortliste von Maximilian Wittig übernommen
+# https://playwright.dev/docs/locators#locate-by-text
+
