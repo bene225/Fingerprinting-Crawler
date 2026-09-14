43d98a4a45a218b143cf6a9beb9ad616a0861c26
Bene H <benehartl22@gmail.com>
2026-09-14 19:40:37 +0200

    Hinzugefügt try_accept partial

--- core/consent.py
+++ core/consent.py
@@ -1,7 +1,11 @@
 import asyncio
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, Page
 import random
 
 # Wortliste von Maximilian Wittig übernommen
 # https://playwright.dev/docs/locators#locate-by-text
 
+
+async def try_accept(page : Page) -> bool:
+    try:
+        await page.get_by_text(Füllen).click()
