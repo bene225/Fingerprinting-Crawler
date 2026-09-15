7aaa99ddcb71568a40009fa5d6a36f95b19dc80c
Bene H <benehartl22@gmail.com>
2026-09-15 10:18:20 +0200

    Remove Debug var consent

--- core/consent.py
+++ core/consent.py
@@ -17,7 +17,6 @@ with open("utils/tcf_terms.json", "r", encoding="utf-8") as tcf_file:
     tcf_accept_chance = tcf_terms_data["posTerms"]
     for tcf_accept_language in tcf_accept_chance:
         tcf_accept_chance_flat = tcf_accept_chance_flat + tcf_accept_chance[tcf_accept_language]
-    print(tcf_accept_chance_flat)
 
 async def try_accept(page : Page) -> bool:
     try:
