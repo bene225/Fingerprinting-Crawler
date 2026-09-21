a84eb2b9b8e46c3beb8d6e45ed2443a2754e860a
Bene H <benehartl22@gmail.com>
2026-09-21 18:27:55 +0200

    EchterTestcrawl über mehrere Websites Methode + Liste an Besuchbaren Websites

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import asyncio
 from core.browser_controller import BrowserController
+from config import CrawlConfig
+
+TEST_DOMAINS = [
+    # Banner im Hauptdokument (CSS-Treffer)
+    "otto.de", "mediamarkt.de", "obi.de",
+    # Sourcepoint-Banner im Iframe
+    "spiegel.de", "zeit.de", "bild.de", "heise.de", "theguardian.com", "bbc.com",
+    # weitere deutsche Nachrichten- und Shopseiten
+    "t-online.de", "web.de", "tagesschau.de", "kicker.de", "chip.de", "focus.de",
+    "welt.de", "computerbase.de", "idealo.de", "check24.de", "kleinanzeigen.de", "thalia.de",
+    # Kontrollseiten ohne Banner und ohne Tracker (Erwartung: Consent False, kaum Events)
+    "example.com", "wikipedia.org", "duckduckgo.com",
+    # bekannter Sonderfall: Bot-Fehlerseite (testet die Fehlerbehandlung)
+    "ebay.de",
+    # Weiterleitung auf eine andere Domain (testet die Berechnung von main_site)
+    "youtu.be",
+] #KI
+
+async def crawl ():
+    async with BrowserController(headless=CrawlConfig.headless, browser_type=CrawlConfig.BrowserType, allow_3p=CrawlConfig.allow_3p):
+        
+
 
 async def testcrawl():
     async with BrowserController(headless = False, allow_3p = False) as bc:
