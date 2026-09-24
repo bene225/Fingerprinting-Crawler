8066857f8d2abb44335a0b460d51bfc669373784
Bene H <benehartl22@gmail.com>
2026-09-24 12:19:47 +0200

    Logging für Website Aufrufe

--- core/logging.py
+++ core/logging.py
@@ -7,3 +7,8 @@ def write_config(config : CrawlConfig):
         with open(config.path_to_output, "a", encoding="utf-8") as out_file:
             out_file.write(dumps(config_asdict, default=str ) + "\n")
         
+def write_website(config : CrawlConfig, events):
+    events_asdict = [asdict(event) for event in events]
+    with open(config.path_to_output, "a", encoding="utf-8") as out_file:
+                out_file.write(dumps(events_asdict, default=str ) + "\n")
+                
