c28a6cd533c35f1c4ca97d777fca2a12efbab6fe
Bene H <benehartl22@gmail.com>
2026-09-24 13:33:38 +0200

    website logging fertig

--- core/logging.py
+++ core/logging.py
@@ -2,13 +2,14 @@ from config import CrawlConfig
 from dataclasses import asdict
 from json import dumps
 
-def write_config(config : CrawlConfig):
+def write_config(config : CrawlConfig) -> None:
         config_asdict = asdict(config)
         with open(config.path_to_output, "a", encoding="utf-8") as out_file:
             out_file.write(dumps(config_asdict, default=str ) + "\n")
         
-def write_website(id : int, website : str, config : CrawlConfig, events : list):
+def write_website(id : int, website : str, config : CrawlConfig, events : list) -> None:
     events_asdict = [asdict(event) for event in events]
+    crawl_result = {"id" : id, "website" : website, "events" : events_asdict}
     with open(config.path_to_output, "a", encoding="utf-8") as out_file:
-                out_file.write(dumps(events_asdict, default=str ) + "\n")
+                out_file.write(dumps(crawl_result, default=str ) + "\n")
                 
